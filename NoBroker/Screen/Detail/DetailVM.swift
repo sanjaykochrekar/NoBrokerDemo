dece84ecddcbc6acf5922b85fba587801e86525f
sanjay <kochrekarsanjay87@gmail.com>
2023-12-30 20:31:31 +0530

    updated detail vm

--- NoBroker/Screen/Detail/DetailVM.swift
+++ NoBroker/Screen/Detail/DetailVM.swift
@@ -10,12 +10,10 @@
 class DetailVM: SectionViewModel {
     var data: [SectionModel] = []
     
-    let product: Product!
     
+    //TODO: Since the data getting from Home screen as Detail screen used same data
     
     init(product: Product) {
-        self.product = product
-        
         let imageList = product.images.map { DetailImageListItem(image: $0) }
         let imageSection = SectionModel(row: imageList)
         data.append(imageSection)
@@ -23,13 +21,18 @@ class DetailVM: SectionViewModel {
         
         let title = DetailTitleListItem(title: product.title)
         let description = DetailDescription(description: product.description)
+        
+        let brand  = DetailDescription(description: "BRAND:  \(product.brand)")
+        
+        let price = DetailDescription(description: "PRICE:  \(product.price)")
+        
+        let discountPercentage = DetailDescription(description: "DISCOUNT:  \(product.discountPercentage)%")
+        
+        let rating = DetailDescription(description: "RATING:  \(product.rating)%")
      
-        let infoSection = SectionModel(row: [title, description])
+        let infoSection = SectionModel(row: [title, description, brand, price, discountPercentage, rating])
         data.append(infoSection)
         
-        
     }
-    
-    
     
 }
