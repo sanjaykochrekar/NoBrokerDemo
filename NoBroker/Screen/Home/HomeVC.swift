2c248b661e296fc2614eb47025e4819cc37a4b33
sanjay <kochrekarsanjay87@gmail.com>
2024-01-02 18:54:31 +0530

    Home Screen code refactoring

--- NoBroker/Screen/Home/HomeVC.swift
+++ NoBroker/Screen/Home/HomeVC.swift
@@ -8,100 +8,67 @@
 import UIKit
 
 
-class HomeVC: UIViewController {
+
+class HomeVC: UITableViewController {
     weak var coordinator: MainCoordinator!
-    var homeView: HomeView!
     lazy var vm = HomeVM()
+    
+    private lazy var searchController = UISearchController(searchResultsController: nil)
     
     
     override func loadView() {
         super.loadView()
-        
-        homeView = HomeView()
-        view.addSubview(homeView)
-        view.backgroundColor = .appBg
-        
-        homeView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            homeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            homeView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            homeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            homeView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        homeView.searchBar.delegate = self
-        homeView.tableView.register(ProductTVCell.self, forCellReuseIdentifier: "ProductTVCell")
-        homeView.tableView.dataSource = self
-        homeView.tableView.delegate = self
+        
+        view.backgroundColor = .appBg
+        tableView.register(ProductTVCell.self, forCellReuseIdentifier: "ProductTVCell")
+        
         vm.delegate = self
         vm.fetchData()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        navigationAndSearchSetup()
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
+    private func navigationAndSearchSetup() {
+        title = "Products"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
+}
+
+
+extension HomeVC: UISearchResultsUpdating {
     
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
-    
-    
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            homeView.tableViewBottomConstraint.constant = -keyboardSize.height
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else { return }
+        
+        if searchText.isEmpty {
+            vm.cancelSearch()
+        } else {
+            vm.searchProduct(searchText)
         }
     }
     
-    
-    @objc func keyboardWillHide(notification: NSNotification) {
-        homeView.tableViewBottomConstraint.constant = 0
-    }
-    
 }
 
 
 
-//MARK: UISearchBarDelegate methods
-extension HomeVC: UISearchBarDelegate {
+//MARK: UITableView dataSource methods
+extension HomeVC {
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        vm.searchProduct(searchBar.text ?? "")
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-        searchBar.searchTextField.text = ""
-        vm.cancelSearch()
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-    }
-    
-}
-
-
-
-//MARK: UITableViewDataSource methods
-extension HomeVC: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         vm.getRowCount(section: section)
     }
     
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let item = vm.getItemAtIndex(indexPath: indexPath) else { return UITableViewCell() }
         let cell = tableView.dequeueReusableCell(withIdentifier: item.identifier, for: indexPath) as! ListCell
         cell.populate(data: item)
@@ -109,7 +76,7 @@ extension HomeVC: UITableViewDataSource {
     }
     
     
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let lastSectionIndex = tableView.numberOfSections - 1
         let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
         if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex && vm.continueLoading && !vm.isSearching {
@@ -119,11 +86,11 @@ extension HomeVC: UITableViewDataSource {
             if vm.getRowCount(section: indexPath.section) < indexPath.row + 3 {
                 vm.fetchData()
             }
-            homeView.tableView.tableFooterView = spinner
-            homeView.tableView.tableFooterView?.isHidden = false
+            tableView.tableFooterView = spinner
+            tableView.tableFooterView?.isHidden = false
         } else if !vm.continueLoading || vm.isSearching{
-            homeView.tableView.tableFooterView?.isHidden = true
-            homeView.tableView.tableFooterView = nil
+            tableView.tableFooterView?.isHidden = true
+            tableView.tableFooterView = nil
         }
     }
     
@@ -131,10 +98,10 @@ extension HomeVC: UITableViewDataSource {
 
 
 
-//MARK: UITableViewDelegate methods
-extension HomeVC: UITableViewDelegate {
+//MARK: UITableView delegate methods
+extension HomeVC {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         guard let product = vm.getItemAtIndex(indexPath: indexPath) as? Product else { return }
         coordinator.showDetailOf(product: product)
@@ -149,7 +116,7 @@ extension HomeVC: HomeVMDelegate {
     
     func reloadTableView() {
         DispatchQueue.main.async { [weak self] in
-            self?.homeView.tableView.reloadData()
+            self?.tableView.reloadData()
         }
     }
     
